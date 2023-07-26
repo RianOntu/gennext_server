767d5fd5ac77c4dd6571af9e594c2b7427b3e846
RianOntu <rianontu13@gmail.com>
2023-07-26 20:31:01 +0600

    adding user to database

--- index.js
+++ index.js
@@ -24,14 +24,16 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    const usersCollection = client.db("task_management").collection("usersCollection")
+    app.post('/adduser',async(req,res)=>{
+      const user=req.body;
+      const result=await usersCollection.insertOne(user);
+      res.send(result)
+    })
+    
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    
   }
 }
 run().catch(console.dir);
